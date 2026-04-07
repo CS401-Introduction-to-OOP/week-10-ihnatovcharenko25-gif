d3d503bca8f78ac3c07acb76cbd1b3efa4c75098
ihnatovcharenko25-gif <ihnat.ovcharenko.25@kse.org.ua>
2026-04-07 19:21:01 +0300

    Event and EventLog

---
+++ ConsoleApp1/Event.cs
@@ -0,0 +1,18 @@
+﻿namespace ConsoleApp1;
+
+public class Event
+{
+    public int NumberMove { get; private set; }
+    public string Description { get; private set; }
+    public string Type { get; private set; }
+    public string Change { get; private set; }
+
+    public Event(int numberMove, string description, string type, string change)
+    {
+        NumberMove = numberMove;
+        Description = description;
+        Type = type;
+        Change = change;
+    }
+    
+}
---
+++ ConsoleApp1/EventLog.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+
+namespace ConsoleApp1;
+
+public class EventLog : IEnumerable<Event>
+{
+    private List<Event> _events;
+
+    public EventLog()
+    {
+        _events = new List<Event>();
+    }
+
+    public void Add(Event e)
+    {
+        _events.Add(e);
+    }
+    public IEnumerator<Event> GetEnumerator()
+    {
+        foreach (Event e in _events)
+            yield return e;
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    
+}
