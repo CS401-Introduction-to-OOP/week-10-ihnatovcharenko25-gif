9fa563bbdf181cd6b07aceeee5ac8b516c6d1a60
ihnatovcharenko25-gif <ihnat.ovcharenko.25@kse.org.ua>
2026-04-07 19:19:52 +0300

    Character and Party

---
+++ ConsoleApp1/Character.cs
@@ -0,0 +1,26 @@
+﻿namespace ConsoleApp1;
+
+public class Character
+{
+    public string Name { get; private set; }
+    public string Class { get; private set; }
+    public uint Rank { get; private set; }
+    public uint Health { get; private set; }
+    public uint Gold { get; private set; }
+    public string State { get; private set; }
+
+    public Character(string name, string aclass, uint rank, uint health, uint gold, string state)
+    {
+        Name = name;
+        Class = aclass;
+        Rank = rank;
+        Health = health;
+        Gold = gold;
+        State = state;
+    }
+
+    public override string ToString()
+    {
+        return $"{Name}, {Class}";
+    }
+}
---
+++ ConsoleApp1/Party.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+
+namespace ConsoleApp1;
+
+public class Party : IEnumerable<Character>
+{
+    private List<Character> _characters;
+    private int _iterator = 0;
+
+    public Party()
+    {
+        _characters = new List<Character>();
+    }
+
+    public void Add(Character character)
+    {
+        _characters.Add(character);
+    }
+    
+    public IEnumerator<Character> GetEnumerator()
+    {
+        foreach (Character character in _characters)
+            yield return character;
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+    
+    public IEnumerator<Character> GetIAliveEnumerator()
+    {
+        foreach (Character c in _characters)
+            if (c.State == "Alive")
+                yield return c;
+    }
+    
+    public IEnumerator<Character> GetLowHpEnumerator(int hp)
+    {
+        foreach (Character c in _characters)
+            if (c.Health < hp)
+                yield return c;
+    }
+
+    public IEnumerator<Character> WhereLevelHigher(int level)
+        => _characters.Where<Character>(c => c.Rank > level).GetEnumerator();
+    
+    public IEnumerator<Character> OrderByLevel()
+        => _characters.OrderBy(c => c.Rank).GetEnumerator();
+
+    public Character MaxLevel()
+        => _characters.MaxBy(c => c.Rank);
+
+
+}
